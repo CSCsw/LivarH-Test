713068c7a59b5345d639a1423a3d3787057de547
Mu Wang <wang970@tsinghua.cs.purdue.edu>
2014-04-14 23:21:03 -0400

    Allow the test /others/mesh to test preaccumulation

--- others/mesh/autotest.sh
+++ others/mesh/autotest.sh
@@ -10,15 +10,17 @@ echo "$testfunc   Error";
 
 trap 'trapFunc $testfunc ; exit' EXIT ERR
 for testMethod in {0,1}; do
+  for testPreacc in {0,1}; do
     for testIndex in {0,1}; do
         make > temp.out
-        command="./meshHess 0 $testMethod 0 $testIndex";
+        command="./meshHess 0 $testMethod $testPreacc $testIndex";
 #        echo "$command";
         $command > temp.out;
         sh -c "sort hess.out > hess.sorted";
         sh -c "sort edge.out > edge.sorted";
-        [[ `diff hess.sorted edge.sorted` ]] &&  (echo "wrong!") || (echo "meshHess($testMethod, 0, $testIndex) OK!");
+        [[ `diff hess.sorted edge.sorted` ]] &&  (echo "wrong!";exit) || (echo "meshHess($testMethod, $testPreacc, $testIndex) OK!");
     done
+  done
 done
 rm meshHess *.out *.sorted *.tap
 trap '' EXIT
