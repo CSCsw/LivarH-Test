64784831fbb7cb59f8adbc230337604c97e29a3c
Mu Wang <wang970@tsinghua.cs.purdue.edu>
2015-11-03 22:38:19 -0500

    minor changes

--- autotest.sh
+++ autotest.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 . ./adpath.sh
-
 for testfunc in ./functions/testF1.cpp ./functions/testF2.cpp ./functions/testF3.cpp ./functions/testF4.cpp ; do
   echo "testing: $testfunc"
   export LD_LIBRARY_PATH=$AD_LIBS_PATH:$OLD_LD_PATH
-  for testmethod in COMPUTE_FULL_HESS LIVARH DIRECT INDIRECT; do
+  for testmethod in COMPUTE_FULL_HESS DIRECT INDIRECT LIVARH; do
     command="$CXX -D $testmethod $CXXFLAGS -I$AD_INCL_PATH -o hessTest hessTest.cpp $testfunc -L$AD_LIBS_PATH -ladolc"; 
     #echo "$command";
     $command
